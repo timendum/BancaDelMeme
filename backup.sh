e70caecb10bcce0c247863ebfa43321716f4d4b5
Timendum <timedum@gmail.com>
2021-02-01 09:30:40 +0100

    Bakcup: migrated

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-DBDIR=./src/db/
-cd $DBDIR
-BACKUP_DIR=./src/db/backup
+cd /home/ambrogio/bancaDelMeme
+DBDIR=./db/
+BACKUP_DIR=./db/backup
 mkdir -p BACKUP_DIR
 today=$(date "+%Y-%m-%d-%H-%M-%S")
 find "$BACKUP_DIR" -type f -mtime +31 -delete
-sqlite3 db.sqlite3 ".backup $BACKUP_DIR/$today.sqlite3"
+sqlite3 $DBDIR/db.sqlite3 ".backup $BACKUP_DIR/$today.sqlite3"
 gzip -f $BACKUP_DIR/$today.sqlite3
 rclone sync $BACKUP_DIR  megabckp:
