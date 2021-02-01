2834808ec19e0c152a3f85fe3dbe6df2fcee0625
Timendum <timedum@gmail.com>
2021-02-01 09:25:00 +0100

    start -> screen (from tmux)

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "Trying to create a backups folder..."
-mkdir backups
-_now=$(date +"%m_%d_%Y")
-_file="/tmp/backup_$_now.sql"
-echo "Starting backup to $_file..."
-docker exec memeinvestor_bot_mysql_1 mysqldump mysql -u mysql -p'strongpass!word' -r "$_file"
-echo "Done dumping the database. Copying the file to the host machine..."
-docker cp memeinvestor_bot_mysql_1:$_file $1
-echo "Done"
+DBDIR=./src/db/
+cd $DBDIR
+BACKUP_DIR=./src/db/backup
+mkdir -p BACKUP_DIR
+today=$(date "+%Y-%m-%d-%H-%M-%S")
+find "$BACKUP_DIR" -type f -mtime +31 -delete
+sqlite3 db.sqlite3 ".backup $BACKUP_DIR/$today.sqlite3"
+gzip -f $BACKUP_DIR/$today.sqlite3
+rclone sync $BACKUP_DIR  megabckp:
--- startbancadelmeme.sh
+++ startbancadelmeme.sh
@@ -1,38 +1,28 @@
 #!/bin/bash
-tmux kill-session -t mememain
-tmux kill-session -t memesub
-tmux kill-session -t memecalc
-#tmux kill-session -t memepayroll
+screen -S mememain -X quit
+screen -S memesub -X quit
+screen -S memecalc -X quit
+screen -S memeteleg -X quit
+
+# crontab
+# cd /home/ambrogio/bancaDelMeme/src && ../bin/python buyabler.py
 
 /bin/sleep 1
-source /home/ribre/.bashrc
-/usr/bin/tmux new-session -d -s mememain
-/usr/bin/tmux send-keys -t mememain "source /home/ribre/.bashrc" C-m
-/usr/bin/tmux send-keys -t mememain "cd BancaDelMeme" C-m
-/usr/bin/tmux send-keys -t mememain "cd src" C-m
-/usr/bin/tmux send-keys -t mememain "python3 main.py" C-m
+screen -d -m -S mememain /bin/bash
+screen -S mememain -X stuff "cd /home/ambrogio/bancaDelMeme/src/^M"
+screen -S mememain -X stuff "../bin/python3 main.py^M"
 
 /bin/sleep 1
-source /home/ribre/.bashrc
-/usr/bin/tmux new-session -d -s memesub
-/usr/bin/tmux send-keys -t memesub "source /home/ribre/.bashrc" C-m
-/usr/bin/tmux send-keys -t memesub "cd BancaDelMeme" C-m
-/usr/bin/tmux send-keys -t memesub "cd src" C-m
-/usr/bin/tmux send-keys -t memesub "python3 submitter.py" C-m
+screen -d -m -S memesub /bin/bash
+screen -S memesub -X stuff "cd /home/ambrogio/bancaDelMeme/src/^M"
+screen -S memesub -X stuff "../bin/python3 submitter.py^M"
 
 /bin/sleep 1
-source /home/ribre/.bashrc
-/usr/bin/tmux new-session -d -s memecalc
-/usr/bin/tmux send-keys -t memecalc "source /home/ribre/.bashrc" C-m
-/usr/bin/tmux send-keys -t memecalc "cd BancaDelMeme" C-m
-/usr/bin/tmux send-keys -t memecalc "cd src" C-m
-/usr/bin/tmux send-keys -t memecalc "python3 calculator.py" C-m
+screen -d -m -S memecalc /bin/bash
+screen -S memecalc -X stuff "cd /home/ambrogio/bancaDelMeme/src/^M"
+screen -S memecalc -X stuff "../bin/python3 calculator.py^M"
 
-#/bin/sleep 1
-#source /home/ribre/.bashrc
-#/usr/bin/tmux new-session -d -s memepayroll
-#/usr/bin/tmux send-keys -t memepayroll "source /home/ribre/.bashrc" C-m
-#/usr/bin/tmux send-keys -t memepayroll "cd BancaDelMeme" C-m
-#/usr/bin/tmux send-keys -t memepayroll "cd src" C-m
-#/usr/bin/tmux send-keys -t memepayroll "python3 payroll.py" C-m
-
+/bin/sleep 1
+screen -d -m -S memeteleg /bin/bash
+screen -S memeteleg -X stuff "cd /home/ambrogio/bancaDelMeme/src/^M"
+screen -S memeteleg -X stuff "../bin/python3 telegram_bot.py^M"
