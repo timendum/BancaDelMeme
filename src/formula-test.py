7863d892c735e5a9495e23ec6bf17fc3800467f2
Timendum <timedum@gmail.com>
2021-02-02 19:02:14 +0100

    Formula test: case net_worth = 0

--- src/formula-test.py
+++ src/formula-test.py
@@ -1,35 +1,39 @@
 # Interactive command line tool for performing test calculations for investment return
 import argparse
 from formula import calculate
+
 try:
     import matplotlib.pyplot as plt
 except ImportError:
     from unittest.mock import MagicMock
+
     plt = MagicMock()
 
 
 def main():
     # == Parse initial and final upvotes from command line
     parser = argparse.ArgumentParser(
-        description=
-        'Interactive command line tool for performing test calculations for investment return.')
-    parser.add_argument('u_init',
-                        metavar='u_init',
-                        type=int,
-                        help='Post upvotes at time of investment.')
-    parser.add_argument('u_final',
-                        metavar='u_final',
-                        type=int,
-                        help='Post upvotes at maturation of investment.')
-    parser.add_argument('invested',
-                        metavar='invested',
-                        type=int,
-                        help='% of MemeCoins Invested in post, base on the net worth.')
-    parser.add_argument('net_worth', metavar='net_worth', type=int, help='Account\'s net worth.')
-    parser.add_argument("-s",
-                        "--save-img",
-                        help="Save generated plot to file 'investment_performance.png'",
-                        action="store_true")
+        description="Interactive command line tool for performing test calculations for investment return."
+    )
+    parser.add_argument(
+        "u_init", metavar="u_init", type=int, help="Post upvotes at time of investment."
+    )
+    parser.add_argument(
+        "u_final", metavar="u_final", type=int, help="Post upvotes at maturation of investment."
+    )
+    parser.add_argument(
+        "invested",
+        metavar="invested",
+        type=int,
+        help="% of MemeCoins Invested in post, base on the net worth.",
+    )
+    parser.add_argument("net_worth", metavar="net_worth", type=int, help="Account's net worth.")
+    parser.add_argument(
+        "-s",
+        "--save-img",
+        help="Save generated plot to file 'investment_performance.png'",
+        action="store_true",
+    )
     args = parser.parse_args()
 
     print("Starting Upvotes:", args.u_init)
@@ -45,46 +49,50 @@ def main():
     print("Factor:", factor)
     print("Base Factor:", calculate(args.u_final, args.u_init, 0))
 
-    #Generate data for performance plot
-    olds = list(range(args.u_init, args.u_final * 2,
-                      args.net_worth))  # + list(range(args.u_final, args.u_final * 2, 10))
-    x = []
-    x2 = []
-    y = []
-    y2 = []
-    for i in olds:
-        x.append(i)
-        result = calculate(i, args.u_init, args.net_worth) * args.invested
-        y.append(result)
-        if i <= args.u_final:
-            x2.append(i)
-            y2.append(result)
+    if args.net_worth:
+        # Generate data for performance plot
+        olds = list(
+            range(args.u_init, args.u_final * 2, args.net_worth)
+        )  # + list(range(args.u_final, args.u_final * 2, 10))
+        x = []
+        x2 = []
+        y = []
+        y2 = []
+        for i in olds:
+            x.append(i)
+            result = calculate(i, args.u_init, args.net_worth) * args.invested
+            y.append(result)
+            if i <= args.u_final:
+                x2.append(i)
+                y2.append(result)
 
-    #Generate performance plot
-    fig = plt.figure(figsize=(7, 7))
-    plt.grid(color='k', alpha=0.15, which='major')
-    plt.title('Investment Performance', fontsize='18')
-    plt.xlabel('Upvotes', fontsize='14')
-    plt.ylabel('Return / M¢', fontsize='14')
-    plt.plot(x, y, label='Return curve')
-    plt.plot(x2, y2, label='Meme Performance', color='r')
-    plt.plot(args.u_final,
-             inv_return,
-             color='r',
-             marker='X',
-             markersize=8,
-             linestyle='none',
-             label='Return: %i M¢ (Profit: %i M¢)' % (inv_return, profit))
-    plt.legend(fontsize='12')
+        # Generate performance plot
+        fig = plt.figure(figsize=(7, 7))
+        plt.grid(color="k", alpha=0.15, which="major")
+        plt.title("Investment Performance", fontsize="18")
+        plt.xlabel("Upvotes", fontsize="14")
+        plt.ylabel("Return / M¢", fontsize="14")
+        plt.plot(x, y, label="Return curve")
+        plt.plot(x2, y2, label="Meme Performance", color="r")
+        plt.plot(
+            args.u_final,
+            inv_return,
+            color="r",
+            marker="X",
+            markersize=8,
+            linestyle="none",
+            label="Return: %i M¢ (Profit: %i M¢)" % (inv_return, profit),
+        )
+        plt.legend(fontsize="12")
 
-    #Interactive show plot
-    plt.show()
+        # Interactive show plot
+        plt.show()
 
-    #Optional save plot
-    if args.save_img:
-        out_filename = 'investment_performance.png'
-        print("Saving image to: '", out_filename, "'")
-        fig.savefig(out_filename)
+        # Optional save plot
+        if args.save_img:
+            out_filename = "investment_performance.png"
+            print("Saving image to: '", out_filename, "'")
+            fig.savefig(out_filename)
 
 
 if __name__ == "__main__":
