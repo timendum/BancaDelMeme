842eb72a23b03818fd99f96697d4b53cb92e22ad
Timendum <timedum@gmail.com>
2021-02-02 08:59:08 +0100

    update start

--- startbancadelmeme.sh
+++ startbancadelmeme.sh
@@ -25,8 +25,3 @@ screen -S memesub -X stuff "../bin/python3 submitter.py^M"
 screen -d -m -S memecalc /bin/bash
 screen -S memecalc -X stuff "cd ./src/^M"
 screen -S memecalc -X stuff "../bin/python3 calculator.py^M"
-
-/bin/sleep 1
-screen -d -m -S memeteleg /bin/bash
-screen -S memeteleg -X stuff "cd ./src/^M"
-screen -S memeteleg -X stuff "../bin/python3 telegram_bot.py^M"
