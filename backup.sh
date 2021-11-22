14032512c4a827a9aa423b25e43bcea4d84cc5ac
Timendum <timedum@gmail.com>
2021-11-22 14:15:52 +0100

    Backup fix path, exclude season backup

--- backup.sh
+++ backup.sh
@@ -5,9 +5,9 @@ cd $script_dir
 
 DBDIR=./db/
 BACKUP_DIR=./db/backup
-mkdir -p BACKUP_DIR
+mkdir -p $BACKUP_DIR
 today=$(date "+%Y-%m-%d-%H-%M-%S")
-find "$BACKUP_DIR" -type f -mtime +31 -delete
+find "$BACKUP_DIR" -type f -mtime +31 ! -name "stagione*" -delete
 sqlite3 $DBDIR/db.sqlite3 ".backup $BACKUP_DIR/$today.sqlite3"
 gzip -f $BACKUP_DIR/$today.sqlite3
 rclone sync $BACKUP_DIR  megabckp:
